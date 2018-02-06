78281a1502ca7b6ee3b0bc50f490d9a2d5221b9d
[a644358] Thibault jeanpierre <thibault.jeanpierre@atos.net>
2018-02-06 14:52:52 +0100

    Fix Global Design
    and change font

--- src/app/main/getting-started/getting-started.component.ts
+++ src/app/main/getting-started/getting-started.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
   selector: 'c3m-getting-started',
   templateUrl: './getting-started.component.html',
-  styleUrls: ['./getting-started.component.css']
+  styleUrls: ['./getting-started.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GettingStartedComponent implements OnInit {
 
